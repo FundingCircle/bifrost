10ec656ca5231e7075f890cc288bcb45acf7bf1f
Dayo Oliyide <dayo.oliyide@fundingcircle.com>
2015-07-22 12:21:27 +0100

    Interposing logged AWS keys. Upgraded Clojure to 1.7.0

--- src/uswitch/bifrost/main.clj
+++ src/uswitch/bifrost/main.clj
@@ -17,6 +17,19 @@
     :validate [string?]]
    ["-h" "--help"]])
 
+(defn partial-interpose [s]
+  (let [first-char (first s)
+        length (- (count s) 2)
+        interposed (clojure.string/join (repeat length \X))
+        last-char (last s)]
+    (str first-char interposed last-char)))
+
+
+(defn interpose-credentials [{:keys [access-key secret-key] :as credentials}]
+  (-> credentials
+      (update :access-key partial-interpose)
+      (update :secret-key partial-interpose)))
+
 (defn credentials
   [{:keys [access-key secret-key]}]
   {:access-key (or (System/getenv "AWS_ACCESS_KEY_ID") access-key)
@@ -32,6 +45,6 @@
       (info "Bifrost" (current-version))
       (when (current-build-number)
         (gauge "build-number" (current-build-number)))
-      (info "Starting Bifrost with config" config)
+      (info "Starting Bifrost with config" (update-in config [:credentials] interpose-credentials))
       (start (make-system config))
       (wait!))))
